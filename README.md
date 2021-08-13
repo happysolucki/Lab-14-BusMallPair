293636a992fbeacffc06d5e0c3187f73e939620f
Lizzy Wiley <Lizzyrho@gmail.com>
2021-08-13 08:29:31 -0500

    changes

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Lab-14-BusMallPair
+
+This collaberation was completed by Brandon Keefer and Lizzy Rhodes.
+
+
+
+
+
+
+ Observations overall: There is a lot more that I need to review as far as objects and functions are concerned. the protoype method was difficult for me to understand since using ES6 classes was easier to understand. some things to remember for next time:
+
+ 1. Don't get thrown by the Protoypes, they are just methods inside of our constructor notation
+
+ 2.   These are the two most **important** terms when using local storage: *JSON.stringify* is to turn our data into a useable string and *JSON.parse* is to turn our data back into JS data.
+
+ 3. The *for/in* statement loops through the **properties of an object**.
+
+ 4. 'tableBody.innerHTML = "";' <=== adding an empty string in HTML will  delete what is selected.
+
--- starter-code/js/catalog.js
+++ starter-code/js/catalog.js
@@ -10,10 +10,10 @@ const cart = new Cart([]);
 function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById("items");
-  for (let i in Product.allProducts) {
+  for (let i in Product.allProducts) { // loop through all the properties of the Product
     // store name of Product in allProducts array to productName
-    let productName = Product.allProducts[i].name;
-    let option = document.createElement("option"); // created an option element
+    let productName = Product.allProducts[i].name; // grab the product names from all the product indexes
+    let option = document.createElement("option"); // created an option element inside of our HTML.
     console.log(productName); //debugger
 
     // set value & text of created option element to productName
@@ -21,7 +21,7 @@ function populateForm() {
     option.textContent = productName;
 
     // append option element to dropdown
-    selectElement.append(option);
+    selectElement.append(option); // all of our product names are inside of the option
   }
 }
 
@@ -41,26 +41,26 @@ function handleSubmit(event) {
 
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
-  // TODO: suss out the item picked from the select list
-  let addItems = document.querySelector("#items");
+  // TODO: get the item picked from the select list
+  let addItems = document.querySelector("#items"); 
   let quantityOfItems = document.querySelector("#quantity");
 
   // TODO: get the quantity
-  let quantityValue = parseInt(quantityOfItems.value);
+  let quantityValue = parseInt(quantityOfItems.value); //change the string to a number we can use
 
   // TODO: using those, add one item to the Cart
-  cart.addItem(addItems.value, quantityValue);
+  cart.addItem(addItems.value, quantityValue); // adding the product and quantity of the product to the cart
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
 function updateCounter() {
   // capture element responsible for showing item count
-  let itemCount = document.querySelector("#itemCount");
+  let itemCount = document.querySelector("#itemCount"); // grab our HTML element to store on screen
   // make variable that'll hold sum of items
   let sumOfItems = 0;
   for (let i = 0; i < cart.items.length; i++) {
     // continuously add quantity of each cart item in cart.items array to sumOfItems
-    sumOfItems += cart.items[i].quantity;
+    sumOfItems += cart.items[i].quantity; // each time we add another item (or several items), the sum will increase
   }
   // update text of itemCount with total quantity of items
   itemCount.textContent = sumOfItems;
@@ -69,16 +69,16 @@ function updateCounter() {
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
   // TODO: Get the item and quantity from the form
-  let cartContent = document.querySelector("#cartContents");
-  let addUpdateItems = document.querySelector("#items");
-  let addQuantityOfItems = document.querySelector("#quantity");
-  let itemName = document.createElement("h4");
+  let cartContent = document.querySelector("#cartContents"); // located inside of our HTML div
+  let addUpdateItems = document.querySelector("#items"); // located inside of our HTML div
+  let addQuantityOfItems = document.querySelector("#quantity"); // located inside of our HTML div
+  let itemName = document.createElement("h4"); // created an element for the name of the product
 
   // update the text inside of our header with submitted product name & value
   itemName.textContent = `${addUpdateItems.value} : ${addQuantityOfItems.value}`;
 
   // TODO: Add a new element to the cartContents div with that information
-  cartContent.append(itemName);
+  cartContent.append(itemName); // our product and quantity was stored in itemName, now it is appended inside of our div!
 
   // Reset form when item is added
   catalogForm.reset();
